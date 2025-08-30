a5d712f77974e055a6a3c4fb21c0e638d5b51709
pasokata <84432010+pasokata@users.noreply.github.com>
2025-08-31 00:49:35 +0900

    switch tab esc

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/one_shot/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/one_shot/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q        , KC_W     , KC_F     , KC_P         , KC_G         ,                                 KC_J     , KC_L     , KC_U     , KC_Y     , KC_SCLN,
     KC_A        , KC_R     , KC_S     , KC_T         , KC_D         ,                                 KC_H     , KC_N     , KC_E     , KC_I     , KC_O   ,
     KC_Z        , KC_X     , KC_C     , KC_V         , KC_B         ,                                 KC_K     , KC_M     , KC_COMM  , KC_DOT   , KC_QUOT,
-    G(KC_L)     , KC_PSCR  , _______  , KC_LSFT      , LA_NAV      , KC_TAB,               KC_SPC,   LA_SYM    , XXXXXXX  , XXXXXXX  , XXXXXXX  , SCRL_TO
+    G(KC_L)     , KC_PSCR  , _______  , KC_LSFT      , LA_NAV      , KC_ESC,               KC_SPC,   LA_SYM    , XXXXXXX  , XXXXXXX  , XXXXXXX  , SCRL_TO
   ),
   [MOU] = LAYOUT_universal( 
     _______  , _______  , _______  , _______  , _______  ,                            _______  , _______  , _______  , _______  , _______  ,
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB      , KC_LBRC  , KC_LCBR  , KC_LPRN      , KC_TILD      ,                                KC_HASH   , KC_RPRN  , KC_RCBR  , KC_RBRC  , KC_GRV ,
     KC_MINS     , KC_ASTR  , KC_EQL   , KC_UNDS      , KC_CIRC      ,                                KC_DLR    , OS_CTRL  , OS_SHFT  , OS_ALT   , OS_CMD ,
     KC_PLUS     , KC_PIPE  , KC_AT    , KC_SLSH      , KC_PERC      ,                                XXXXXXX   , KC_BSLS  , KC_AMPR  , KC_EXLM  , KC_QUES,
-    _______     , MACRO1   , _______  , _______      , _______      , VIMCW    ,          _______ ,  _______   , XXXXXXX  , XXXXXXX  , XXXXXXX  , _______
+    _______     , MACRO1   , _______  , _______      , _______      , KC_TAB    ,          _______ ,  _______   , XXXXXXX  , XXXXXXX  , XXXXXXX  , _______
   ),
   [NAV] = LAYOUT_universal(
     KC_ESC      , SW_WIN   , SW_TAB_L , SW_TAB_R     , KC_VOLU      ,                                KC_DOWN   , KC_RGHT  , KC_HOME  , KC_END   , KC_DEL,
